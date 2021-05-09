3069c5f400ede2004d8622b2e8f679e7bed3685d
Данила Комлев <m1902452@edu.misis.ru>
2021-05-09 08:54:17 +0300

    fa

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import ReactDOM from 'react-dom'
 
 import {createUser, createHabbit, getAllHabit} from "./APIHelper.js"
@@ -31,7 +31,7 @@ const initializeAssistant = (getState /*: any*/) => {
         token:
           process.env.REACT_APP_TOKEN  ??
           "",
-        initPhrase: ``,
+        initPhrase: `Запусти Habit-tracker`,
         getState,
       });
     }
@@ -50,22 +50,67 @@ function App() {
     //Хуки данных пользователя
     const [userId, setUserId] = useState("test_userId");
     const [sberId, setSberId] = useState(123);
-    const [userName, setUserName] = useState("Кирилл");
+    const [userName, setUserName] = useState(" ̶К̶и̶р̶и̶л̶л̶  Данила");
     const [userAge, setUserAge] = useState(20);
     const [userGender, setUserGender] = useState("М");
 
     //Хуки создания новой привычки
     const[createHabitName, setCreateHabitName] = useState("");
     const[createHabitCount, setCreateHabitCount] = useState(66);
+    const[userHabits, setUserHabits]= useState([])
 
     //Хуки состояния текущих привычек
     const [checkboxStates, setCheckboxStates] = useState([false,true,false,false,false]);
 
-    //Хук с привычками
-    const [habits, setHabits] = useState({});
-
     //Ассистент Sber
-    const assistant = initializeAssistant(() => {getStateForAssistant()} );
+    const assistant = useRef();
+
+    useEffect(()=>{
+      assistant.current=initializeAssistant(() => getStateForAssistant());
+      assistant.current.on("start", (event) => {
+        console.log(`assistant.on(start)`, event);
+        
+      });
+      assistant.current.on(
+        "data",
+        (event /*: any*/) => {
+          if(event.type=="smart_app_data"){
+            console.log("User")
+            console.log(event)
+            if (event.sub != undefined) {
+              console.log("Sub", event.sub)
+              setUserId(event.sub)
+              createUser(event.sub, sberId, userName, userAge, userGender)
+              var habits =  getAllHabit(event.sub)
+              console.log(habits)
+            }else if (event.user_id != undefined) {
+              console.log("UserId", event.user_id)
+              setUserId(event.user_id)
+              createUser(event.userId, sberId, userName, userAge, userGender)
+            }
+          };
+          console.log(`assistant.on(data)`, event);
+          const { action } = event;
+
+          dispatchAssistantAction(action);
+        }
+      );
+
+    },[]);
+
+
+    const dispatchAssistantAction = async (action) => {
+      console.log("dispatchAssistantAction", action);
+      if (action) {
+        switch (action.type) {
+          case "something":
+            //something
+            break;
+          default:
+            break;
+        }
+      }
+    };
     
     const getStateForAssistant = () =>{
         console.log('getStateForAssistant: state:', state)
@@ -86,68 +131,20 @@ function App() {
 
 
     //
-    useEffect(()=>{
-      assistant.current=initializeAssistant(() => getStateForAssistant());
-      assistant.current.on("start", (event) => {
-        console.log(`assistant.on(start)`, event);
-        
-      });
-      assistant.current.on(
-        "data",
-        (event /*: any*/) => {
-          if(event.type=="smart_app_data"){
-            console.log("User")
-            console.log(event)
-            if (event.sub != undefined) {
-              console.log("Sub", event.sub)
-              setUserId(event.sub)
-              createUser(event.sub, sberId, userName, userAge, userGender)
-            }else if (event.user_id != undefined) {
-              console.log("UserId", event.user_id)
-              setUserId(event.user_id)
-              createUser(event.userId, sberId, userName, userAge, userGender)
-            }
-          };
-          console.log(`assistant.on(data)`, event);
-          const { action } = event;
-
-          dispatchAssistantAction(action);
-        }
-      );
-
-    },[]);
-    
-    const dispatchAssistantAction = async (action) => {
-      console.log("dispatchAssistantAction", action);
-      if (action) {
-        switch (action.type) {
-          case "something":
-            //something
-            break;
-          default:
-            break;
-        }
-      }
-    };
-    
-    
-    //Заполнение полей с привычкам
-    setHabits( getAllHabit(userId) );
-    console.log(habits);
-
+ 
+      
+    useEffect(() => {}, [])
 
     //Обратотчики рользовательской активности
+
     const createHabbitAction = () =>{
       createHabbit(userId, createHabitName, createHabitCount);
       setCreateHabitName('');
       setCreateHabitCount(66);
-    };
-    
-    const updateActivity = (HabitId, DateOfActivity, State) =>
-    {
-
-
     }
+
+
+            
       return (
           <React.Fragment>
             
@@ -156,11 +153,11 @@ function App() {
                 <HabitCreationButton onClick={()=> setPopupActive(!isPopupActive)}/>
               </HabitHeader >
 
-              <HabitBox habitName={'Бросить курить'} 
-              progressValue={33} 
-              maxValue={66} 
-              habitProgress = {checkboxStates} 
-              deleteHabit = {()=>{console.log('habit delete')}}/>
+              <HabitBox habitName={'Бросить университет'} progressValue={33} maxValue={66} habitProgress = {checkboxStates} deleteHabit = {()=>{console.log('habit delete')}}/>
+              <HabitBox habitName={'Бросить университет'} progressValue={33} maxValue={66} habitProgress = {checkboxStates} deleteHabit = {()=>{console.log('habit delete')}}/>
+              <HabitBox habitName={'Бросить университет'} progressValue={33} maxValue={66} habitProgress = {checkboxStates} deleteHabit = {()=>{console.log('habit delete')}}/>
+              <HabitBox habitName={'Бросить университет'} progressValue={33} maxValue={66} habitProgress = {checkboxStates} deleteHabit = {()=>{console.log('habit delete')}}/>
+              <HabitBox habitName={'Бросить университет'} progressValue={33} maxValue={66} habitProgress = {checkboxStates} deleteHabit = {()=>{console.log('habit delete')}}/>
             
               {isPopupActive ?
               <div>
